2c2a45d4794ae42ab700076d3ed4b57634508bb5
zmajkic <zeljko.majkic@telekom.de>
2021-05-18 15:56:34 +0200

    prepared for sql injection

--- src/main/java/de/telekom/jdbcchallenge/Main.java
+++ src/main/java/de/telekom/jdbcchallenge/Main.java
@@ -2,6 +2,7 @@ package de.telekom.jdbcchallenge;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -10,7 +11,7 @@ import java.util.Scanner;
 public class Main {
 
 	java.util.Scanner scanner = new java.util.Scanner(System.in);
-	
+
 	final static String DRIVER = "org.mariadb.jdbc.Driver";
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
@@ -29,36 +30,40 @@ public class Main {
 			System.out.println("Nachname: " + resultSet.getString(4)); // Nachname
 
 		}
-		
-		   Scanner scanner = new Scanner(System.in);
-	       System.out.println("ID: " );
-           long id = Long.parseLong(scanner.nextLine());
-           System.out.println("Anrede: " );
-           short anrede = Short.parseShort(scanner.nextLine());
-           System.out.println("Vorname: " );
-           String vorname = scanner.nextLine();
-           System.out.println("Nachname: " );
-           String nachname = scanner.nextLine();
-           
-           System.out.println( id +  " " + anrede + " " + vorname + " " + nachname + " " );
-           
-          statement.executeQuery( "INSERT INTO personen (ID, ANREDE, VORNAME, NACHNAME) VALUES  ("+ id + ", " + anrede + ", '"+ vorname + "', '" + nachname + "')");
-   
-      	resultSet = statement.executeQuery("select * from personen");
-  		while (resultSet.next()) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("ID: ");
+		long id = Long.parseLong(scanner.nextLine());
+		System.out.println("Anrede: ");
+		short anrede = Short.parseShort(scanner.nextLine());
+		System.out.println("Vorname: ");
+		String vorname = scanner.nextLine();
+		System.out.println("Nachname: ");
+		String nachname = scanner.nextLine();
+		scanner.close();
+		System.out.println(id + " " + anrede + " " + vorname + " " + nachname + " ");
+
+
+		PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO personen ( ID, ANREDE, VORNAME, NACHNAME) VALUES ( ?, ?, ?, ? )");
+		preparedStatement.setLong(1, id );
+		preparedStatement.setShort(2, anrede );
+		preparedStatement.setString(3, vorname );		
+		preparedStatement.setString(4, nachname );		
+		preparedStatement.execute();
+				
+//		statement.executeQuery("INSERT INTO personen (ID, ANREDE, VORNAME, NACHNAME) VALUES  (" + id + ", " + anrede
+//				+ ", '" + vorname + "', '" + nachname + "')");
+
+		resultSet = statement.executeQuery("select * from personen");
+		while (resultSet.next()) {
 			System.out.println("ID: " + resultSet.getLong(1)); // ID
 			System.out.println("Anrede: " + resultSet.getShort(2)); // Anrede
 			System.out.println("Vorname: " + resultSet.getString(3)); // Vorname
 			System.out.println("Nachname: " + resultSet.getString(4)); // Nachname
 
 		}
-          
-          
-          
-           
 		resultSet.close();
 		statement.close();
 		connection.close();
 	}
-		        
 }
